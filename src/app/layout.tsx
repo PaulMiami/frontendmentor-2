55cfc9c4cfd0760a36d7880281f0fad059391f09
PaulMiami <pbiccherai@hotmail.com>
2022-11-25 17:03:10 -0500

    added font + colors + screens sizes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
+import "./globals.css";
+
 import type React from "react";
 
-import "./globals.css";
+import { League_Spartan } from "@next/font/google";
+
+const leagueSpartan = League_Spartan({
+  subsets: ["latin"],
+  variable: "--font-league-spartan",
+  weight: "500",
+});
 
 export default function RootLayout({
   children,
@@ -14,7 +22,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className={`${leagueSpartan.variable} font-sans`}>{children}</body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    screens: {
+      sm: "375px",
+      md: "786px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        desaturatedDarkCyan: "hsl(180, 29%, 50%)",
+        lightGrayishCyan: "hsl(180, 52%, 96%)",
+        lighterGrayishCyan: "hsl(180, 81%, 95%)",
+        darkGrayishCyan: "hsl(180, 8%, 52%)",
+        veryDarkGrayishCyan: "hsl(180, 14%, 20%)",
+      },
+      fontFamily: {
+        sans: ["var(--font-league-spartan)", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
